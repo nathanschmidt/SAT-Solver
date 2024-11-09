2cf319950da7158191129081a35bca63e725e867
Nathan Schmidt <nathan.schmidt@tum.de>
2024-11-09 23:52:51 +0100

    sat: rectify optimizer, add optimizer correctness proof, define minimal form

--- Project/Sat.v
+++ Project/Sat.v
@@ -73,50 +73,56 @@ Fixpoint interp (V : valuation) (p : form) : bool :=
     | form_var x => V x
     | <{ true }> => true
     | <{ false }> => false
-    | <{ p /\ q }> => (interp V p) && (interp V q)
+    | <{ p /\ q }> => (interp V p) && (interp V q) (* both arguments are fully computed to true or false before function is applied *)
     | <{ p \/ q }> => (interp V p) || (interp V q)
     | <{ p -> q }> => (negb (interp V p)) || (interp V q)
     | <{ ~p }> => negb (interp V p)
-    end.
+    end.    
 
+(* Difficulty: first wrote all cases for and, or and impl directly without pattern matching on the optimized version, leaving out potential for
+   optimization and therefore trying the apply_optim fixpoint which is not possible in Coq. *)
+(* Now bottom-up approach *)
 Fixpoint optim (p : form) : form :=
     match p with
-    | <{ q /\ true }> => optim q
-    | <{ true /\ q }> => optim q
-    | <{ _ /\ false }> => <{ false }>
-    | <{ false /\ _ }> => <{ false }>
     | <{ q /\ q' }> =>
         let q_opt := optim q in 
         let q'_opt := optim q' in
-        <{ q_opt /\ q'_opt }>
-    | <{ _ \/ true }> => <{ true }>
-    | <{ true \/ _ }> => <{ true }>
-    | <{ q \/ false }> => optim q
-    | <{ false \/ q }> => optim q
+        match q_opt, q'_opt with
+        | <{ true }>, _ => q'_opt
+        | _, <{ true }> => q_opt
+        | <{ false }>, _ => <{ false }>
+        | _, <{ false }> => <{ false }>
+        | _, _ => <{ q_opt /\ q'_opt }>
+        end
     | <{ q \/ q' }> =>
         let q_opt := optim q in
         let q'_opt := optim q' in
-        <{ q_opt \/ q'_opt }>
-    | <{ true -> q }> => optim q
-    | <{ _ -> true }> => <{ true }>
-    | <{ false -> _ }> => <{ true }>
-    (* | <{ q -> false }> => optim <{ ~q }> *)
+        match q_opt, q'_opt with
+        | <{ true }>, _ => <{ true }>
+        | _, <{ true }> => <{ true }>
+        | <{ false }>, _ => q'_opt
+        | _, <{ false }> => q_opt
+        | _, _ => <{ q_opt \/ q'_opt }>
+        end
     | <{ q -> q' }> =>
         let q_opt := optim q in
         let q'_opt := optim q' in
-        <{ q_opt -> q'_opt }>
-    | <{ ~true }> => <{ false }>
-    | <{ ~false }> => <{ true }>
+        match q_opt, q'_opt with
+        | <{ true }>, _ => q'_opt
+        | _, <{ true }> => <{ true }>
+        | <{ false }>, _ => <{ true }>
+        | _, <{ false }> => <{ ~q_opt }>
+        | _, _ => <{ q_opt -> q'_opt }>
+        end
     | <{ ~q }> =>
         let q_opt := optim q in
-        <{ ~q_opt }>
+        match q_opt with
+        | <{ true }> => <{ false }>
+        | <{ false }> => <{ true }>
+        | _ => <{ ~q_opt }>
+        end
     | _ => p
     end.
-
-Theorem optim_correct : forall V p,
-    interp V p = interp V (optim p).
-Proof.
-    (* TODO *) Admitted.
 
 (* Difficulty: fixpoint termination => doesn't work *)
 (* Fixpoint apply_optim (p : form) :=
@@ -125,3 +131,102 @@ Proof.
         then p
     else
         apply_optim (p_opt). *)
+
+Hint Resolve andb_true_r : core.
+Hint Resolve andb_false_r : core.
+Hint Resolve orb_true_r : core.
+Hint Resolve orb_false_r : core.
+
+Ltac destruct_if b q := 
+    let rw :=
+        repeat match goal with
+        | [ H : _ = _ |- _ ] => rewrite H
+        end in
+    destruct b; rw;
+    [ reflexivity |
+      destruct q; try destruct b; reflexivity]. 
+
+(* Approach: first manual proofs of whole project, then automization., then Ltac *)
+Theorem optim_correct_old : forall V p,
+    interp V p = interp V (optim p).
+Proof.
+    induction p as [ x | b | q IHq q' IHq' | q IHq q' IHq' | q IHq q' IHq' |
+                    q IHq]; 
+    try reflexivity.
+    - simpl. destruct (optim q) as [x | b | q_opt1 q_opt2 | q_opt1 q_opt2 |
+                                    q_opt1 q_opt2 | q_opt];
+      try (destruct (optim q') as [x' | b' | q'_opt1 q'_opt2 | q'_opt1 q'_opt2 |
+                                  q'_opt1 q'_opt2 | q'_opt];
+          try (simpl in *; rewrite IHq, IHq'; reflexivity);
+          destruct b'; rewrite IHq, IHq'; auto).
+        + destruct_if b (optim q').
+    - simpl. destruct (optim q) as [x | b | q_opt1 q_opt2 | q_opt1 q_opt2 |
+                                    q_opt1 q_opt2 | q_opt];
+        try (destruct (optim q') as [x' | b' | q'_opt1 q'_opt2 | q'_opt1 q'_opt2 |
+                                  q'_opt1 q'_opt2 | q'_opt];
+          try destruct b';
+          simpl in *; rewrite IHq, IHq'; auto;
+        try (destruct b; 
+            [simpl in *; rewrite IHq, IHq'; reflexivity |
+             destruct (optim q'); try destruct b;
+              rewrite IHq, IHq'; reflexivity]);
+        destruct (optim q') as [x' | b' | q'_opt1 q'_opt2 | q'_opt1 q'_opt2 |
+                                  q'_opt1 q'_opt2 | q'_opt];
+          try destruct b';
+          simpl in *; rewrite IHq, IHq'; auto).
+        + destruct_if b (optim q').
+    - simpl. destruct (optim q) as [x | b | q_opt1 q_opt2 | q_opt1 q_opt2 |
+                                    q_opt1 q_opt2 | q_opt];
+      try (destruct b; 
+            [simpl in *; rewrite IHq, IHq'; reflexivity |
+             destruct (optim q'); try destruct b;
+              rewrite IHq, IHq'; reflexivity]);
+        destruct (optim q') as [x' | b' | q'_opt1 q'_opt2 | q'_opt1 q'_opt2 |
+                                  q'_opt1 q'_opt2 | q'_opt];
+          try destruct b';
+          simpl in *; rewrite IHq, IHq'; auto.
+    - simpl. destruct (optim q) as [x | b | q_opt1 q_opt2 | q_opt1 q_opt2 |
+                                    q_opt1 q_opt2 | q_opt];
+        try destruct b;
+        rewrite IHq; reflexivity.
+    Qed.
+
+(* TODO: make look nicer *)
+Theorem optim_correct : forall V p,
+    interp V p = interp V (optim p).
+Proof.
+    induction p as [ x | b | q IHq q' IHq' | q IHq q' IHq' | q IHq q' IHq' |
+                    q IHq]; 
+    try reflexivity;
+    simpl; destruct (optim q) as [x | b | q_opt1 q_opt2 | q_opt1 q_opt2 |
+                                    q_opt1 q_opt2 | q_opt];
+    try destruct_if b (optim q');
+    try destruct b;
+    try (rewrite IHq; reflexivity);
+    destruct (optim q');
+    try (simpl in *; rewrite IHq, IHq'; reflexivity);
+    try destruct b;
+    simpl in *; rewrite IHq, IHq'; auto.
+    Qed.
+
+Fixpoint contains_no_atoms (f : form) : bool :=
+    match f with
+    | form_var _ => true
+    | <{ p /\ q }> | <{ p \/ q }> | <{ p -> q }> => 
+        contains_no_atoms p && contains_no_atoms q
+    | <{ ~p }> => contains_no_atoms p
+    | _ => false
+    end.
+
+Definition minimal_form (f : form) : Prop :=
+    (exists b, f = form_bool b) \/ contains_no_atoms f = true.
+
+Theorem optim_minimizes : forall (f : form),
+    minimal_form (optim f).
+Proof.
+    (* TODO *) Admitted.
+
+(* Idea: theorem proving that this is the simplest form 
+    check if predicate valid, with predicate being certain constructs not contained in form 
+    i.e. either only true or false or no atoms (true or false) contained *)
+
